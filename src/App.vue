cca247d907c5b419c8b77c8c12d9b038858293fe
TNsaturday <tnsaturday@gmail.com>
2020-09-05 09:10:22 +0300

    Add videoList component (stub), import axios

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,32 @@
     <h1>Youtube player</h1>
 
     <SearchBar @termChange="onTermChange"/>
+    <VideoList />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+const API_KEY = 'AIzaSyAWoIq4zXcPkI1Dsz-6qIhGcerTT2L6tes';
 
 export default {
   name: 'App',
   components: {
     SearchBar,
+    VideoList,
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => console.log(response));
     }
   }
 };
